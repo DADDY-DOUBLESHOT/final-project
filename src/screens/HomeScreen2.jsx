84f71a1859cf03f18024ca13157c7fff32384c96
Pratham Sonawane <78498799+DADDY-DOUBLESHOT@users.noreply.github.com>
2023-02-16 02:51:08 +0530

    Home screen started

--- src/screens/HomeScreen2.jsx
+++ src/screens/HomeScreen2.jsx
@@ -1,62 +1,130 @@
 import React, { useState } from "react";
 import {
+  FlatList,
+  Image,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
+  Dimensions,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import Carousel,{ ParallaxImage } from "react-native-snap-carousel-expo-46-compatible";
+
+const { width: screenWidth } = Dimensions.get("window");
+
+const CarouselCardItem = ({ item, index },parallaxProps) => {
+  return (
+    <View style={styles.item}>
+                <ParallaxImage
+                    source={{ uri: item.imgUrl }}
+                    containerStyle={styles.imageContainer}
+                    style={styles.image}
+                    parallaxFactor={0.4}
+                    {...parallaxProps}
+                />
+            </View>
+  );
+};
 
 const HomeScreen2 = ({ navigation }) => {
   const [searchTerm, setSearchTerm] = useState("");
-
+  const isCarousel = React.useRef(null);
   const handleSearch = () => {
     navigation.navigate("BookSearch", { searchTerm });
     setSearchTerm("");
   };
+  const data = [
+    {
+      title: "Aenean leo",
+      body: "Ut tincidunt tincidunt erat. Sed cursus turpis vitae tortor. Quisque malesuada placerat nisl. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
+      imgUrl: "https://picsum.photos/id/11/200/300",
+    },
+    {
+      title: "In turpis",
+      body: "Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ",
+      imgUrl: "https://picsum.photos/id/10/200/300",
+    },
+    {
+      title: "Lorem Ipsum",
+      body: "Phasellus ullamcorper ipsum rutrum nunc. Nullam quis ante. Etiam ultricies nisi vel augue. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.",
+      imgUrl: "https://picsum.photos/id/12/200/300",
+    },
+  ];
 
   return (
-
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <TouchableOpacity
-            style={styles.searchBar}
-            onPress={() => navigation.navigate("BookSearch")}
-          >
-            <Ionicons
-              name="search"
-              size={24}
-              color="black"
-              style={styles.searchIcon}
-            />
-            <Text style={styles.searchText}>Search any book</Text>
-          </TouchableOpacity>
-        </View>
-        <Text style={styles.bodyText}>Welcome to my app!</Text>
-        <Text> Home from Navigation</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity
+          style={styles.searchBar}
+          onPress={() => navigation.navigate("BookSearch")}
+        >
+          <Ionicons
+            name="search"
+            size={24}
+            color="black"
+            style={styles.searchIcon}
+          />
+          <Text style={styles.searchText}>Search any book</Text>
+        </TouchableOpacity>
       </View>
+      <View style={styles.trendingContainer}>
+        <Text style={styles.trendingText}>Top Trending</Text>
+        <Carousel
+          ref={isCarousel}
+          data={data}
+          renderItem={CarouselCardItem}
+          sliderWidth={screenWidth}
+        sliderHeight={screenWidth}
+        itemWidth={screenWidth - 60}
+          hasParallaxImages={true}
+        />
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: "flex",
+    flexDirection: "column",
   },
   header: {
     height: 60,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-    justifyContent: 'center',
-    alignItems: 'center',
+    borderBottomColor: "#ccc",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  trendingContainer: {
+    marginTop: 10,
+    backgroundColor: "#fff",
+    display: "flex",
+    flexDirection: "column",
+  },
+  trendingText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginHorizontal: 10,
+    marginVertical: 5,
+  },
+  trendingItem: {
+    marginHorizontal: 5,
+    width: 120,
+    height: 120,
+    backgroundColor: "#ccc",
+    justifyContent: "center",
+    alignItems: "center",
   },
   searchBar: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#eee',
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#eee",
     borderRadius: 10,
-    width: '90%',
+    width: "90%",
     height: 40,
     paddingLeft: 10,
   },
@@ -65,12 +133,29 @@ const styles = StyleSheet.create({
   },
   searchText: {
     fontSize: 16,
-    color: '#aaa',
+    color: "#aaa",
   },
   bodyText: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     marginTop: 30,
+    backgroundColor: "red",
+  },
+});
+const card_styles = StyleSheet.create({
+  item: {
+    width: screenWidth - 60,
+    height: screenWidth - 60,
+  },
+  imageContainer: {
+    flex: 1,
+    marginBottom: Platform.select({ ios: 0, android: 1 }), // Prevent a random Android rendering issue
+    backgroundColor: 'white',
+    borderRadius: 8,
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    resizeMode: 'cover',
   },
 });
 
